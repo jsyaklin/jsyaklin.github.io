52e7a00a24c6df306a4ab3189d860829bd56a262
John Yaklin <foowheely@gmail.com>
2025-01-21 22:23:10 -0600

    updated for Fall 2024 Robot Day

---
+++ year-counter.js
@@ -0,0 +1,18 @@
+
+var startDate = new Date("May 31, 2018 12:00:00 GMT-4:00").getTime();
+
+var yearsField = document.getElementById("years");
+
+function updateTimer()
+{
+    var now = new Date().getTime();
+
+    var distance = now - startDate;
+
+    // if (distance < 0) distance = 0;
+
+    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365.25));
+
+    yearsField.innerHTML = years.toString();
+}
+setInterval(updateTimer, 1000);
